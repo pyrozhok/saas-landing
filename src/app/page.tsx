a74ae1b89eced55c875b54e82a357cfe1bde5aa3
Aleksandr Sopin <alex.sopin0803@gmail.com>
2025-05-10 14:39:33 +0700

    fix: add margin to prevent cutting of design images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Hero from '@/components/hero';
+import Logos from '@/components/logos';
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Logos />
+    </>
+  );
 }
