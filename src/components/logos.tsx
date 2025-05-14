ab1bd029ecf36f6cf429d334c30cc509a55e23d6
Aleksandr Sopin <alex.sopin0803@gmail.com>
2025-05-14 18:33:51 +0700

    feat: add animation

--- src/components/logos.tsx
+++ src/components/logos.tsx
@@ -1,3 +1,9 @@
+'use client';
+
+import { Fragment } from 'react';
+import Image from 'next/image';
+import { motion } from 'motion/react';
+
 import quantumLogo from '@/assets/icons/quantum-logo.svg';
 import acmeLogo from '@/assets/icons/acme-corp-logo.svg';
 import echoValleyLogo from '@/assets/icons/echo-valley-logo.svg';
@@ -6,7 +12,6 @@ import outsideLogo from '@/assets/icons/outside-logo.svg';
 import apexLogo from '@/assets/icons/apex-logo.svg';
 import celestialLogo from '@/assets/icons/celestial-logo.svg';
 import twiceLogo from '@/assets/icons/twice-logo.svg';
-import Image from 'next/image';
 
 const logosImages = [
   { name: 'Quantum', image: quantumLogo },
@@ -26,12 +31,26 @@ const Logos = () => {
         <h3 className='text-center text-xl tracking-wide text-white/50'>
           Already chosen by these market leaders
         </h3>
-        <div className='mt-12 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]'>
-          <div className='flex gap-24 pr-24'>
-            {logosImages.map(({ name, image }) => (
-              <Image key={name} src={image} alt={name} />
+        <div className='mt-12 flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]'>
+          <motion.div
+            animate={{
+              x: '-50%',
+            }}
+            transition={{
+              duration: 30,
+              ease: 'linear',
+              repeat: Infinity,
+            }}
+            className='flex flex-none gap-24 pr-24'
+          >
+            {Array.from({ length: 2 }).map((_, i) => (
+              <Fragment key={i}>
+                {logosImages.map(({ name, image }) => (
+                  <Image key={name} src={image} alt={name} />
+                ))}
+              </Fragment>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
